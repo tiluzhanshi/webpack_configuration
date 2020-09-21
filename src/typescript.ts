4fe48abcafc800361358c0a515ffbbc4f8ab3078
tiluzhanshi <hi_beijing@126.com>
2020-09-22 05:43:03 +0800

    delete

--- src/typescript.ts
+++
@@ -1,9 +0,0 @@
-interface Foo {
-    name: string,
-    age:number
-}
-
-let obj:Foo = {
-    name:"string",
-    age:123
-};
--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,46 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 const {
-    merge
-} = require("webpack-merge");
-const loaderConfig = require('./configuration/loader.config');
-const commonConfig = require("./configuration/common.config");
-module.exports = merge(commonConfig, loaderConfig);
+    CleanWebpackPlugin
+} = require("clean-webpack-plugin");
+module.exports = {
+    target: "web",
+    mode: "development",
+    context: path.resolve(__dirname, "./src"),
+    entry: {
+        index: "./index.js"
+    },
+    output: {
+        filename: '[id].js',
+        path: path.resolve(__dirname, "./dist"),
+    },module: {
+        rules: [
+            /**
+             * babel转换：ES2015,
+             * babel-loader提供了自定义loader函数
+             */
+            {
+                test: /\.js$/,
+                exclude: /(node_modules|bower_components)/,
+                use: [{
+                    // 你也可以通过使用 cacheDirectory 选项，将 babel-loader 提速至少两倍。这会将转译的结果缓存到文件系统中。
+                    loader: 'babel-loader?cacheDirectory',
+                    options: {
+                        presets: ['@babel/preset-env']
+                    }
+                }, ]
+
+            },
+
+        ],
+
+    },
+    plugins: [
+        new CleanWebpackPlugin(),
+        new HtmlWebpackPlugin({
+            filename: "index.html",
+            template: "./index.html"
+        }),
+    ],
+
+}
