a2a2eaa828b9938bde5da3cc5ebb6a1d46864aa7
tiluzhanshi <hi_beijing@126.com>
2020-09-24 11:25:20 +0800

    no message

--- src/index.js
+++ src/index.js
@@ -1,14 +1,1 @@
-const path = require("path");
-require("@babel/register")({
-  // ...
-});
-let path1 = path.join(__dirname,",,","./.","","???");
-
-console.log(path1);
-
-let path2 = path.resolve(__dirname,",,");
-
-console.log(__dirname);
-
-let foo = () => true;
-
+import "./main.scss";
--- webpack.config.js
+++ webpack.config.js
@@ -1,44 +1,61 @@
 const path = require("path");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
 const {
-    CleanWebpackPlugin
+  CleanWebpackPlugin
 } = require("clean-webpack-plugin");
+
 module.exports = {
-    target: "web",
-    mode: "development",
-    context: path.resolve(__dirname, "./src"),
-    entry: {
-        index: "./index.js"
-    },
-    output: {
-        filename: '[id].js',
-        path: path.resolve(__dirname, "./dist"),
-    },module: {
-        rules: [
-            {
-                test: /\.jss$/,
-                exclude: /(node_modules|bower_components)/,
-                use: [{
-                    // 你也可以通过使用 cacheDirectory 选项，将 babel-loader 提速至少两倍。这会将转译的结果缓存到文件系统中。
-                    loader: 'babel-loader?cacheDirectory',
-                    options: {
-                        // 重要的是要注意，@babel/preset-env它不支持stage-x插件。
-                        // 如果设置了.browserlistrc配置文件后，不需要在这里再配置presets了
-                        // presets: ['@babel/preset-env']
-                    }
-                }, ]
-
-            },
-
-        ],
-
-    },
-    plugins: [
-        new CleanWebpackPlugin(),
-        new HtmlWebpackPlugin({
-            filename: "index.html",
-            template: "./index.html"
-        }),
-    ],
-
+  mode: "development",
+  target: "web",
+  context: path.resolve(__dirname, "./src"),
+  entry: "./index.js",
+  module: {
+    rules: [
+      {
+        test: /\.png$/i,
+        use: [
+          {
+            loader: "url-loader",
+            options: {
+              limit: 1,
+              fallback: require.resolve('file-loader'),
+              quality: 85,
+              name:"[name].[ext]",
+              publicPath:"/abc/",
+              outputPath:"images"
+            }
+          },
+        ]
+      },
+      {
+        test: /\.s?css$/,
+        exclude: /node_modules/,
+        use: [
+          // {
+          //   loader:path.resolve(__dirname, "./src/css-loader.js"),
+            
+          // },
+          {
+            loader: "file-loader?name=[name].css"
+          },
+          {
+            loader:"extract-loader",
+          },
+          {
+            loader:"css-loader",
+            options: {
+              importLoaders: 1,
+              url: true,
+            }
+          },
+          {
+            // 自动添加前缀
+            loader: 'postcss-loader'
+          }
+        ]
+      }
+    ]
+  },
+  plugins: [
+    new CleanWebpackPlugin()
+  ]
 }
