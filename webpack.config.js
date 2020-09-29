642caa7d5af56cdbf4bb7e30aaf6227b1ea879bc
tiluzhanshi <hi_beijing@126.com>
2020-09-29 17:15:04 +0800

    no message

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,32 @@
-const {
-    merge
-} = require("webpack-merge");
-const loaderConfig = require('./configuration/loader.config');
-const pluginConfig = require("./configuration/plugins.config");
-const commonConfig = require("./configuration/common.config");
-module.exports = merge(commonConfig, loaderConfig, pluginConfig);
+const path = require("path");const {
+    CleanWebpackPlugin
+} = require("clean-webpack-plugin");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const VueLoaderPlugin = require('vue-loader/lib/plugin');
+module.exports = {
+    target: "web",
+    mode: "development",
+    context: path.resolve(__dirname, "../src"),
+    entry: {
+        index: "./index.js"
+    },
+    output: {
+        filename: '[name][id].js',
+        path: path.resolve(__dirname, "../dist"),
+    },
+    devtool: "source-map",
+    resolveLoader: {
+        modules: [
+            "node_modules",
+            "./loaders"
+        ],
+    },
+    plugins: [
+        new CleanWebpackPlugin(),
+        new HtmlWebpackPlugin({
+            filename: "index.html",
+            template: "./index.html"
+        }),
+        new VueLoaderPlugin(),
+    ]
+}
